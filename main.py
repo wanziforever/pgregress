daddb0df308255a092f7128147e1a831acd83d48
Maggie <sunhuihui@highgo.com>
2019-01-11 14:15:58 +0800

    add the xml parse in the main.py

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 
 import sys
 import os
+import importlib
 from profile import Profile
-from app import Application
+#from multiapp import MultiApplication
 import logging
+import xml.dom.minidom as xmldom
 logger = logging.getLogger('main')
 
 def help():
@@ -15,6 +17,7 @@ logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger('main')
 test_data_dir = './data'
+#strategy_file = './strategy.xml'
 
 def all_profiles():
     """list all the valid test profiles
@@ -34,13 +37,41 @@ def all_profiles():
         dirs.append(path)
         
     return dirs
+def parse_strategy():
+    """parse the strategy xml file
+
+    :rtype: the strategy file for this framework
+    :returns: a dict between strategy name and used applicaiton,profile, checker class name 
+    """
+    global strategy_map
+    strategy_map = dict()
+    
+    filepath=os.path.abspath("strategy.xml")
+    DOMTree = xmldom.parse(filepath)
+    collection = DOMTree.documentElement
+    strategy = collection.getElementsByTagName("strategy")
+    for i in strategy:
+        attr_name = str(i.getAttribute("name"))
+        attr_app = str(i.getAttribute("applicationClass"))
+        attr_profile = str(i.getAttribute("profileClass"))
+        attr_checker = str(i.getAttribute("checkerClass"))
+        strategy_map.update({attr_name:[attr_app,attr_profile,attr_checker]})
+
 
 # main
 if __name__ == "__main__":
+    parse_strategy()
+
     for profile in all_profiles():
         profile = Profile(profile, use_schedule=True)
 
-        app = Application(profile)
+        if profile._ptype not in strategy_map:
+            logger.error("unknown strategy name")
+            return
+        #app = Application(profile)
+        app_mod = importlib.import_module(strategy_map[profile_ptype][0])
+        app = app_mod.Application(profile)
+
         try:
             app.run()
             app.report_gen()
